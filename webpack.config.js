be95bd25bb1cf6d9a6bad2c97691205c4a202b84
elijahprince73 <elijahprince73@gmail.com>
2017-09-28 14:52:58 -0700

    added styles to the form

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,13 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
-    },{
-      test: /\.scss$/,
-       use: [
-         'style-loader',
-         'css-loader',
-         'sass-loader'
-       ]
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',
